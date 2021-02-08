242677a0f393751a681756c091b6fb74e03238ee
Egor Riskovets <eriskovets@gmail.com>
2021-02-09 00:06:09 +0500

    добавил try catch

--- server.js
+++ server.js
@@ -2,18 +2,13 @@ const http = require('http')
 const fs = require('fs')
 const server = http.createServer((req, res) =>{
     console.log(req.url)
-    let body;
-    // const body = req.url === '/styles.css' 
-    // ? fs.readFileSync('./public/styles.css')
-    // : fs.readFileSync('./public/index.html')
-    if(req.url === '/styles.css') {
-        body = fs.readFileSync('./public/styles.css')
-    }
-    else if(req.url === '/js_1.js') {
-        body = fs.readFileSync('./public/js_1.js')
-    }
-    else {
-        body = fs.readFileSync('./public/index.html')
+    let body = null
+    const publicPath = './public'
+    try {
+        body = fs.readFileSync(`${publicPath}${req.url}`)
+    }catch(e) {
+        console.log(e)
+        body = fs.readFileSync(`${publicPath}/index.html`)
     }
     
     res.end(body)
